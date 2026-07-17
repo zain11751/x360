660b7e6cac844cb33812854002ace18cdfcc3ed0
zain11751 <zainbalouch11751@gmail.com>
2026-07-16 23:12:02 -0700

    Increase JSON body limit to 15mb

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3001;
 
 // Middlewares
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '15mb' }));
 
 // Bootstrapping: Ensure at least one active Admin user exists
 async function bootstrapAdmin() {
